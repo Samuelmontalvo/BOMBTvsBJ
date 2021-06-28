3774647c0749e8170bc1a7fe3e41834228ff55ab
Samuelmontalvo <smontalvo@miners.utep.edu>
2021-06-28 12:25:03 -0600

    fixed BJ distance

--- correlation.R
+++ correlation.R
@@ -4,15 +4,15 @@ View(BJ)
 attach(BJ)
 
 # Normality test p<0.05 = not normally distributed
-shapiro.test(Peak_Power)
+shapiro.test(Peak_velocity)
 
 #pearsons correlation
-cor(Distance,Peak_Power)
+cor(Distance,Peak_velocity)
 
 #spearman correlation
 cor(Distance,Peak_Power, method = c("spearman"))
 
+#Scatter Plot
+library(ggplot2)
 
-cor(Distance,Peak_Power, use = "everything",
-    method = c("spearman"))
-
+Peak_velocity
